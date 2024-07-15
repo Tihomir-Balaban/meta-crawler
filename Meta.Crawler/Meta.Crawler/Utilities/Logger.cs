877336c6fd2522bd10994150b3196393bc47c513
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-07-15 17:11:52 +0200

    feat: creating a logger

---
+++ Meta.Crawler/Meta.Crawler/Utilities/Logger.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+namespace Meta.Crawler.Utilities;
+
+internal static class Logger
+{
+    public static void Init() =>
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+    public static void LogInformation(string message)
+        => Log.Information(message);
+    public static void LogWarning(string message)
+        => Log.Warning(message);
+
+    public static void LogError(string message)
+        => Log.Error(message);
+}
