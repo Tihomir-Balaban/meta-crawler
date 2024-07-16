57e71ec9131ce9f5848775194329b9e78a8093b7
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-07-16 21:14:11 +0200

    feat: adding a console app processing animation

---
+++ Meta.Crawler/Meta.Crawler/Utilities/LoadingAnimation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meta.Crawler.Utilities;
+
+internal class LoadingAnimation
+{
+    private readonly char[] _sequence = { '/', '-', '\\', '|' };
+    private int _counter;
+    private bool _active;
+    private Thread _thread;
+
+    public LoadingAnimation()
+    {
+        _counter = 0;
+        _active = false;
+        _thread = new Thread(Spin);
+    }
+
+    public void Start()
+    {
+        if (_active) return;
+
+        _active = true;
+        _thread.Start();
+    }
+
+    public void Stop()
+    {
+        _active = false;
+        _thread.Join();
+        Console.Write("\b");
+    }
+
+    private void Spin()
+    {
+        while (_active)
+        {
+            Console.Write(_sequence[_counter % _sequence.Length]);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            _counter++;
+            Thread.Sleep(100);
+        }
+    }
+}
