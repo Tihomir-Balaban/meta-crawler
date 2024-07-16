0b71f9a88b4abbf4e76bab0c4ee6999cd165f862
Tihomir-Balaban <TihomirBalaban1988@gmail.com>
2024-07-16 21:13:07 +0200

    feat: creating serializable Models

---
+++ Meta.Crawler/Meta.Crawler/Models/Listing.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Meta.Crawler.Models;
+
+[Serializable]
+internal sealed class Listing
+{
+    [JsonProperty("timestamp")]
+    [JsonPropertyName("timestamp")]
+    internal string Timestamp { get; set; }
+
+    [JsonProperty("title")]
+    [JsonPropertyName("title")]
+    internal string Title { get; set; }
+}
---
+++ Meta.Crawler/Meta.Crawler/Models/TvChannel.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Meta.Crawler.Models;
+
+[Serializable]
+internal sealed class TvChannel
+{
+    [JsonProperty("number")]
+    [JsonPropertyName("number")]
+    internal int Number { get; set; }
+
+    [JsonProperty("name")]
+    [JsonPropertyName("name")]
+    internal string Name { get; set; }
+}
